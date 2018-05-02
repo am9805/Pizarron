78a26d21be6fa9d8234c821c22cfa3a7d2898209
Angélica Arroyave <angelica.arroyavem@udea.edu.co>
2018-05-02 09:41:23 -0500

    Idiom toString() has a new implemetation

--- src/java/com/udea/pizarronrows/Figure.java
+++ src/java/com/udea/pizarronrows/Figure.java
@@ -5,10 +5,27 @@
  */
 package com.udea.pizarronrows;
 
-/**
- *
- * @author Asus
- */
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
 public class Figure {
+    private JsonObject json;
     
+    public Figure(JsonObject json){
+        this.json = json;
+    }
+    @Override
+    public String toString() {
+        StringWriter writer = new StringWriter();
+        Json.createWriter(writer).write(json);
+        return writer.toString();
+    }
+
+    public void setJson(JsonObject json) {
+        this.json = json;
+    }
+
+    public JsonObject getJson() {
+        return json;
+    }
 }
