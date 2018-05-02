0203132990f34b1e3833f342baa9737e09f935e6
Angélica Arroyave <angelica.arroyavem@udea.edu.co>
2018-05-02 13:21:03 -0500

    POJO Coordenadas was made

---
+++ src/java/com/udea/pizarronrows/Coordenadas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.pizarronrows;
+
+import java.util.logging.Logger;
+
+public class Coordenadas {
+    private float x;
+    private float y;
+
+    public Coordenadas(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
