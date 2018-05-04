6133efb06b36f935388c8807f300721b6ac6b5f8
santiago <santiago@Santiago-PC>
2018-05-03 21:31:35 -0500

    Implementado el lapiz y sus colores

--- web/pizarron.js
+++ web/pizarron.js
@@ -4,42 +4,43 @@ let save = document.getElementById("save");
 let context = canvas.getContext("2d");
 var isDrawing;
 
-canvas.onmousedown = function(e) {
-  isDrawing = true;
-  context.lineWidth = 0;
-  context.lineJoin = context.lineCap = 'round';
-
-  context.moveTo(e.clientX, e.clientY);
+canvas.onmousedown = function (e) {
+    isDrawing = true;
+    context.lineWidth = 0;
+    context.lineJoin = context.lineCap = 'round';
+    context.moveTo(e.clientX, e.clientY);
 };
-canvas.onmousemove = function(e) {
-  if (isDrawing) {
-    let rect = canvas.getBoundingClientRect();
-    context.lineTo(e.clientX - rect.left, e.clientY - rect.top);
-    context.strokeStyle = 'green';
-    context.stroke();
-    var color = document.inputForm.color;
-    for (i = 0; i < document.inputForm.color.length; i++) {
-        if (document.inputForm.color[i].checked) {
-             color = document.inputForm.color[i];
-            console.log(color.toString());
-            break;
+canvas.onmousemove = function (e) {
+    if (isDrawing) {
+        let rect = canvas.getBoundingClientRect();
+        context.lineTo(e.clientX - rect.left, e.clientY - rect.top);
+        var color = document.inputForm.color;
+        for (i = 0; i < document.inputForm.color.length; i++) {
+            if (document.inputForm.color[i].checked) {
+                color = document.inputForm.color[i];
+                console.log(color.toString());
+                break;
+            }
         }
+        color = color.toString();
+        if (color === 'red') {
+            context.strokeStyle = 'red';
+            } 
+            else if (color === 'blue'){
+                context.strokeStyle = 'blue';
+                } 
+                else if(color === 'orange'){
+                    context.strokeStyle = 'orange';    
+                    } 
+                    else if(color === 'green'){
+                        context.strokeStyle = 'green';    
+                        }
+        context.stroke();
     }
-    switch(color) {
-    case 'red':
-    default:
-        code block
-        break;
-    case 'blue':
-        code block
-        break;
-    case 'orange':
-    case 'green':
-}
-  }
 };
-canvas.onmouseup = function() {
-  isDrawing = false;
+
+canvas.onmouseup = function () {
+    isDrawing = false;
 };
 
 //canvas.addEventListener("mousemove", defineImage, false);
@@ -57,26 +58,18 @@ function getCurrentPos(evt) {
 //Funcion para tomar el color y la forma desde el formulario HTML5
 function defineImage(evt) {
     let currentPos = getCurrentPos(evt);
-    if (evt.buttons !== 1){
+    if (evt.buttons !== 1) {
         return;
     }
     var color = document.inputForm.color;
     for (i = 0; i < document.inputForm.color.length; i++) {
         if (document.inputForm.color[i].checked) {
-             color = document.inputForm.color[i];
+            color = document.inputForm.color[i];
             console.log(color.toString());
             break;
         }
     }
-    for (i = 0; i < document.inputForm.shape.length; i++) {
-        if (document.inputForm.shape[i].checked) {
-            var shape = document.inputForm.shape[i];
-            break;
-        }
-    }
-
     let json = JSON.stringify({
-//  "shape": shape.value,
         "color": color.value,
         "coords": {
             "x": currentPos.x,
@@ -105,8 +98,8 @@ function drawImageText(image) {
  * @param {type} evt
  * @returns {undefined}
  */
-function erase(evt){
-  context.clearRect(0, 0, canvas.width, canvas.height);
+function erase(evt) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 /**
@@ -116,16 +109,16 @@ function erase(evt){
  * @param {type} evt
  * @return {undefined}
  */
-function guardarImagen(evt){
+function guardarImagen(evt) {
     var tipo = "image/png";
     var imagenData = canvas.toDataURL(tipo);
     var imagenDescargada = document.createElement('a');
     imagenDescargada.download = 'myCanvas';
     imagenDescargada.href = imagenData;
-    imagenDescargada.dataset.downloadurl = [tipo, imagenDescargada.download, 
-        imagenDescargada.href].join(':');
+    imagenDescargada.dataset.downloadurl = [tipo, imagenDescargada.download,
+    imagenDescargada.href].join(':');
     document.body.appendChild(imagenDescargada);
     imagenDescargada.click();
     document.body.removeChild(imagenDescargada);
-    
+
 }
